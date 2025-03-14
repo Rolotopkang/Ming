5ac620dd998e26be2c621b029e4823ceb0481b1b
GingerCraft <anran7689@outlook.com>
2025-03-14 17:13:50 -0400

    7777

---
+++ Assets/Invisible_wall.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class Invisible_wall : MonoBehaviour
+{
+
+        void Start()
+        {
+            // 获取 MeshRenderer 组件并禁用它
+            MeshRenderer meshRenderer = GetComponent<MeshRenderer>();
+            if (meshRenderer != null)
+            {
+                meshRenderer.enabled = false;
+            }
+
+            // 确保 Collider 组件仍然启用
+            Collider collider = GetComponent<Collider>();
+            if (collider != null)
+            {
+                collider.enabled = true;
+            }
+        }
+}
+
+
+
